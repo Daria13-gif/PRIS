ec6d4f4920bc47948e441c44f89146c95fbcfce4
Zhizh <Zhizhilevaarina29@gmail.com>
2025-04-30 16:22:00 +0300

    1 lab k8s

---
+++ lr1_k8s/pris-proj/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js in Kubernetes!');
+});
+
+// Добавляем эндпоинт для метрик Prometheus
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.send('# HELP node_app_info Simple Node.js app\n# TYPE node_app_info gauge\nnode_app_info 1');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
